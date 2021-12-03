9fef5f760fc86ab78710c9369ff0da03675f553a
Phil G <fillupgalbraith@gmail.com>
2021-12-03 09:56:36 -0800

    built custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,21 @@
-// write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+class useForm extends React.Component {
+    state = {
+        data,
+        setData
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        setData(
+            axios.get(`http://localhost:3333/plants`)
+              .then( res => res)
+        );
+    };
+    return ( 
+        [data, setData] 
+        )
+}
+
+export default useForm
